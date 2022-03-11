4345cc45a8b5e498cee52b19b29e44d12c308fd3
Sp0ngbik <Sp0ngbik@gmail.com>
2022-03-11 09:47:35 +0300

    sideBar

--- sideBar.js
+++ sideBar.js
@@ -1,11 +1,16 @@
 function openNav() {
-  document.getElementById("mySidebar").style.width = "100%";
-  document.getElementById("shadow_side_bar").style.opacity = "50%";
-  document.getElementById("shadow_side_bar").style.zIndex = "2";
+  document.getElementById("mySidebar").className='sidebarActive';
+  document.getElementById("shadow_side_bar").className = "shadow_side_bar_active";
+  
+  // document.getElementById("mySidebar").style.width = "100%";
+  // document.getElementById("shadow_side_bar").style.opacity = "50%";
+  // document.getElementById("shadow_side_bar").style.zIndex = "2";
 }
 
 function closeNav() {
-  document.getElementById("mySidebar").style.width = "0";
-  document.getElementById("shadow_side_bar").style.opacity = "0%";
-  document.getElementById("shadow_side_bar").style.zIndex = "-1";
+  document.getElementById("mySidebar").className = "sidebarOff";
+  document.getElementById("shadow_side_bar").className = "shadow_side_bar_off";
+
+  // document.getElementById("shadow_side_bar").style.opacity = "0%";  
+  // document.getElementById("shadow_side_bar").style.zIndex = "-1";
 }
