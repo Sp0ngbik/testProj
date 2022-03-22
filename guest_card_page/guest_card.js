199f62f124b932dda116d2f71a2202e901d2ce13
Vlad Ostapuk <Sp0ngbik@gmail.com>
2022-03-22 13:49:17 +0300

    fix

--- guest_card_page/guest_card.js
+++ guest_card_page/guest_card.js
@@ -1,56 +1,55 @@
-
-function mobile_version_balance_info(){
-    $('.balance_cash_process').on('click',function(e){
-      if($(e.currentTarget).hasClass('balance_cash_process_active')){
-            $(e.currentTarget).removeClass('balance_cash_process_active')
-            $(e.currentTarget).children('.balance_log_info_purchase')
-            .removeClass('balance_log_info_purchase_active')
-      }else{
-            $(e.currentTarget).addClass('balance_cash_process_active')
-            $(e.currentTarget).children('.balance_log_info_purchase')
-            .addClass('balance_log_info_purchase_active')
-      }
-    })
+function mobile_version_balance_info() {
+  $(".balance_cash_process").on("click", function (e) {
+    if ($(e.currentTarget).hasClass("balance_cash_process_active")) {
+      $(e.currentTarget).removeClass("balance_cash_process_active");
+      $(e.currentTarget)
+        .children(".balance_log_info_purchase")
+        .removeClass("balance_log_info_purchase_active");
+    } else {
+      $(e.currentTarget).addClass("balance_cash_process_active");
+      $(e.currentTarget)
+        .children(".balance_log_info_purchase")
+        .addClass("balance_log_info_purchase_active");
+    }
+  });
 }
 
-mobile_version_balance_info()
-
+mobile_version_balance_info();
 
 function scrollToTop() {
-    $(".go_to_top").hide();
-    $(window).scroll(function () {
-      if ($(window).scrollTop() > 0) {
-        $("#header_nav_block").addClass("upper_block_container_shadow");
-      } else {
-        $("#header_nav_block").removeClass("upper_block_container_shadow");
-      }
-    });
-    $(window).scroll(function () {
-      let halfSize = $(window).height() / 2;
-      if ($(window).scrollTop() > halfSize) {
-        $(".go_to_top").show();
-      } else {
-        $(".go_to_top").hide(700);
-      }
-    });
-    $(".header").on("click", ".go_to_top", function (e) {
-      $("body, html").animate({ scrollTop: 0 }, 800);
-    });
-  }
-  
-  scrollToTop();
+  $(".go_to_top").hide();
+  $(window).scroll(function () {
+    if ($(window).scrollTop() > 0) {
+      $("#header_nav_block").addClass("upper_block_container_shadow");
+    } else {
+      $("#header_nav_block").removeClass("upper_block_container_shadow");
+    }
+  });
+  $(window).scroll(function () {
+    let halfSize = $(window).height() / 2;
+    if ($(window).scrollTop() > halfSize) {
+      $(".go_to_top").show();
+    } else {
+      $(".go_to_top").hide(700);
+    }
+  });
+  $(".header").on("click", ".go_to_top", function (e) {
+    $("body, html").animate({ scrollTop: 0 }, 800);
+  });
+}
 
+scrollToTop();
 
 function openNav() {
-    document.getElementById("mySidebar").className = "sidebarActive";
-    document.getElementById("shadow_side_bar").className =
-      "shadow_side_bar_active";
-    if ($(window).width() <= "560") {
-      $("openbtn_image").attr("data-change-img", "times-img");
-    }
+  document.getElementById("mySidebar").className = "sidebarActive";
+  document.getElementById("shadow_side_bar").className =
+    "shadow_side_bar_active";
+  if ($(window).width() <= "560") {
+    $("openbtn_image").attr("data-change-img", "times-img");
   }
-  
-  function closeNav() {
-    document.getElementById("mySidebar").className = "sidebarOff";
-    document.getElementById("shadow_side_bar").className = "shadow_side_bar_off";
-  }
+}
+
+function closeNav() {
+  document.getElementById("mySidebar").className = "sidebarOff";
+  document.getElementById("shadow_side_bar").className = "shadow_side_bar_off";
+}
